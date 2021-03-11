3b161ba89d4a467140c63450e5770bb8889fdcb7
vkorat <vivek.korat@saeculumsolutions.com>
2021-03-11 16:29:42 +0530

    commite service and repository and view modal

---
+++ folderst/Demo.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,14 @@
+﻿using Demo.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModal> GetCourses();
+    }
+}
---
+++ folderst/Demo.Application/ViewModels/CourseViewModal.cs
@@ -0,0 +1,14 @@
+﻿using Demo.Dmain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Application.ViewModels
+{
+    public class CourseViewModal
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
