946b5678448aeb5a8a355ad7b71c05c8e31e5819
vkorat <vivek.korat@saeculumsolutions.com>
2021-03-12 10:08:15 +0530

    secure controller commite

---
+++ folderst/demo.mvc/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using Demo.Application.Interfaces;
+using Demo.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demo.mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _CourseService;
+        public CourseController(ICourseService courseService)
+        {
+            _CourseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModal model = _CourseService.GetCourses();
+            return View(model);
+        }
+    }
+}
