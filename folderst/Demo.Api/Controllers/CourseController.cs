3df282e0a926e1a5c2e22c3cdc4a0468f2757ec3
vkorat <vivek.korat@saeculumsolutions.com>
2021-03-12 13:07:06 +0530

    add api project commite

---
+++ folderst/Demo.Api/Controllers/CourseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+
+    }
+}
